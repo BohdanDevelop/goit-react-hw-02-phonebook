41bf60c3ca4139b0c9b6ded54793466842e8543f
BohdanDeveloper <radchukyou1999@gmail.com>
2022-05-22 19:57:33 +0300

    new form repository

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+import Section from "./Section";
+import Form from "./Form"
+
+
+export const App = () => {
+  return (
+    <div>
+     
+<Section>
+  <Form/>
+  
+
+</Section>
+    </div>
+  );
+};
---
+++ src/components/Form/Components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import style from "./Filter.module.scss";
+import { nanoid } from "nanoid";
+import PropTypes from "prop-types";
+
+const Filter = ({handleFilter, filter})=>{
+    
+    return (
+        <label className={style.label}>Filter
+        <input  className={style.input}
+        id="#filter"
+        type="text"
+        onChange={handleFilter}
+        value={filter}
+        />
+        </label>
+      
+    )
+}
+Filter.propTypes = {
+
+filter: PropTypes.string.isRequired,
+handleFilter : PropTypes.func.isRequired,
+onClick: PropTypes.func.isRequired,
+}
+export default Filter;
+
